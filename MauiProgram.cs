efc671862a4e356852d900bf95cfdfef9da59849
Shane Neuville <shneuvil@microsoft.com>
2024-03-25 15:09:38 -0500

    fix

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -17,9 +17,33 @@ public static class MauiProgram
             .ConfigureMauiHandlers(handlers =>
             {
 #if MACCATALYST
+                ButtonHandler.PlatformViewFactory = (x) =>
+                {
+                    var button = new UIKit.UIButton(UIKit.UIButtonType.System);
+
+                    // Starting with iOS 15, we can use the button.Configuration and assign future UIButton.Configuration.ContentInsets here instead of the deprecated UIButton.ContentEdgeInsets.
+                    // It is important to note that the configuration will change any set style changes so we will do this right after creating the button.
+                    if (OperatingSystem.IsIOSVersionAtLeast(15))
+                    {
+                        var config = UIKit.UIButtonConfiguration.PlainButtonConfiguration;
+                        button.Configuration = config;
+                    }
+
+		            UIKit.UIControlState[] ControlStates = { UIKit.UIControlState.Normal, UIKit.UIControlState.Highlighted, UIKit.UIControlState.Disabled };
+                    foreach (UIKit.UIControlState uiControlState in ControlStates)
+                    {
+                        button.SetTitleColor(UIKit.UIButton.Appearance.TitleColor(uiControlState), uiControlState); // If new values are null, old values are preserved.
+                        button.SetTitleShadowColor(UIKit.UIButton.Appearance.TitleShadowColor(uiControlState), uiControlState);
+                        button.SetBackgroundImage(UIKit.UIButton.Appearance.BackgroundImageForState(uiControlState), uiControlState);
+                    }
+
+                    return button;
+
+                };
 				ButtonHandler.Mapper.ReplaceMapping<Button, IButtonHandler>(nameof(Button.ContentLayout), ButtonExtensions.MapContentLayout);
 				ButtonHandler.Mapper.ReplaceMapping<Button, IButtonHandler>(nameof(Button.Text), ButtonExtensions.MapText);
 				ButtonHandler.Mapper.ReplaceMapping<Button, IButtonHandler>(nameof(Button.TextTransform), ButtonExtensions.MapText);
+				ButtonHandler.Mapper.ReplaceMapping<Button, IButtonHandler>(nameof(Button.Padding), ButtonExtensions.MapPadding);
 #elif WINDOWS
                 ButtonHandler.Mapper.AppendToMapping<Button, IButtonHandler>(nameof(Button.ContentLayout), CustomButton.MapContentLayout);
 
